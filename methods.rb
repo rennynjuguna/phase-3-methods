537e38b0a48ea33f36531baeb026ae93f09317ad
Derek Kambo <Tnguyen410@student.gsu.edu>
2023-02-20 14:53:17 +0300

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello #{name}!"
+end
+
+def greet_with_default(name="Hello, programmer!")
+  puts "Hello #{name}!"
+end
+
+def add(num1, num2)
+  num1 + num2
+end
+
+def halve(num)
+  if num.integer? == true
+    num = num/2
+  else
+    nil
+  end
+end
